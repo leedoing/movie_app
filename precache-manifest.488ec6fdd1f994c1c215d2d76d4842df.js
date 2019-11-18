e808121b48aba30e6bf307443f1bfc7ca8bcccc9
leedoing <lluckyy77@gmail.com>
2019-11-18 10:11:43 +0900

    Updates

---
+++ precache-manifest.488ec6fdd1f994c1c215d2d76d4842df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52ae2c2f785abf945f577c3858833dc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "62a2344df7367b6f508d",
+    "url": "/movie_app/static/css/main.9ade48ee.chunk.css"
+  },
+  {
+    "revision": "9747a9dd24c2ec0c2979",
+    "url": "/movie_app/static/js/2.11da3340.chunk.js"
+  },
+  {
+    "revision": "62a2344df7367b6f508d",
+    "url": "/movie_app/static/js/main.3c39a2fe.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
